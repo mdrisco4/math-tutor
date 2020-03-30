4a25d9115857d033780ea125b2d66100725bfcc2
michaeldriscoll <michaelndriscoll81@gmail.com>
2020-03-30 18:10:05 -0400

    getting home page set up and some buttons for different branches of math.  need to set up separate pages and add some event listeners, then set up forms

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+
+
+function Home() {
+    return (
+      <div>
+          <button>arithmetic</button>
+          <button>algebra</button>
+          <button>statistics</button>
+          <button>calculus</button>
+          <button>linear algebra</button>
+      </div>
+    );
+  }
+  
+  export default Home;
